5cdb5f5ff452eef8064652ef9993b2ad313da5b6
su <lukasz.skiba@frontdoor.com>
2024-02-22 00:05:36 +0100

    Category-added

--- schemas/categories/index.ts
+++ schemas/categories/index.ts
@@ -2,5 +2,8 @@ export default {
   name: 'categories',
   type: 'document',
   title: 'Category',
-  fields: [{name: 'name', type: 'string', title: 'Category'}],
+  fields: [
+    {name: 'name', type: 'string', title: 'Category'},
+    {name: 'description', title: 'Description', type: 'text'},
+  ],
 }
